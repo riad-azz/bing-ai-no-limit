3ef16edbf5388972dc3703a617238d8a9e3f097d
Riadh <riadh.azzoun@hotmail.com>
2023-11-20 13:00:50 +0100

    Update Fixed the issue where changing mode would bring back the limitation. Issue : https://github.com/riad-azz/bing-ai-no-limit/issues/6

--- scripts/content.js
+++ scripts/content.js
@@ -16,7 +16,7 @@ async function waitForElement(parent, selector) {
   });
 }
 
-const removeLimitation = async () => {
+const resetInputMaxLength = async () => {
   const mainHost = await waitForElement(document, ".cib-serp-main");
   const mainRoot = mainHost.shadowRoot;
   const actionHost = await waitForElement(mainRoot, "#cib-action-bar-main");
@@ -24,21 +24,53 @@ const removeLimitation = async () => {
   const inputHost = await waitForElement(actionRoot, "cib-text-input");
   const inputRoot = inputHost.shadowRoot;
   const searchInput = await waitForElement(inputRoot, "#searchbox");
+
   searchInput.removeAttribute("maxlength");
+
   const letterCounter = await waitForElement(actionRoot, ".letter-counter");
-  letterCounter.innerHTML = "Unlimited characters by riad-azz";
+  letterCounter.innerHTML = "Character limit removed by riad-azz";
+
+  return searchInput;
+};
+
+
+
+
+const removeLimitation = async () => {
+  const searchInput = await resetInputMaxLength();
   console.log("Character limit was removed.");
 
-  const observer = new MutationObserver((mutationsList) => {
+
+  const config = { attributes: true, childList: false, subtree: false };
+
+  const callback = function (mutationsList, observer) {
     for (const mutation of mutationsList) {
+      if (mutation.type === 'attributes') {
+        searchInput.removeAttribute("maxlength");
+        console.log('Attribute ' + mutation.attributeName + ' changed to: ' + searchInput.getAttribute(mutation.attributeName));
+      }
+    }
+
+  };
+
+  const observer = new MutationObserver(callback);
+
+  // Start observing the target node for configured mutations
+  observer.observe(searchInput, config);
+
+  function watchElementRemoved() {
+
+    const interval = setInterval(function () {
       if (!searchInput.isConnected) {
         removeLimitation();
         observer.disconnect();
+        clearInterval(interval);
+        resolve(true);
       }
-    }
-  });
+    }, 3000);
+  }
 
-  observer.observe(actionRoot, { childList: true });
+  watchElementRemoved();
 };
 
 removeLimitation();
